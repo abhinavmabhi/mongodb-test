0e66df5468e69052f6865b6eeb8401c23cc2cd2e
abhinavmabhi <dexmarketing192018@gmail.com>
2025-03-13 09:30:09 +0530

    updated the design

--- test_project/settings.py
+++ test_project/settings.py
@@ -131,7 +131,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "app" / "static",  # Changed to point to app/static
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- test_project/urls.py
+++ test_project/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.create_item,name="create_item"),
-    path('base/',views.index,name='index'),
-]
+    path('', views.index, name='index'),
+    path('create/', views.create_item, name='create_item'),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
